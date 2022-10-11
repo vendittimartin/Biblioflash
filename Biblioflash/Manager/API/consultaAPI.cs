87ba72ebf41d55424e9ad374c52c0b6e603e97c5
joelb4 <joelb123444@gmail.com>
2022-10-11 18:32:57 -0300

    ConsultaAPI refactoring

--- Biblioflash/Manager/API/consultaAPI.cs
+++ Biblioflash/Manager/API/consultaAPI.cs
@@ -13,6 +13,12 @@ namespace Biblioflash.Manager.API
     {
         public List<LibroDTO> Consulta(string pTituloLibro)
         {
+            dynamic docs = ConsultarApi(pTituloLibro);
+            return ListarLibrosDTO(docs);
+        }
+
+        public dynamic ConsultarApi(string pTituloLibro)
+        {
 
             System.Net.ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
             var url = "http://openlibrary.org/search.json?title=" + pTituloLibro.Replace(" ", "+");
@@ -20,41 +26,10 @@ namespace Biblioflash.Manager.API
             try
             {
                 WebResponse mResponse = Request.GetResponse();
-                using (Stream responseStream = mResponse.GetResponseStream())
-                {
-                    StreamReader reader = new StreamReader(responseStream, Encoding.UTF8);
-                    dynamic mResponseJSON = JsonConvert.DeserializeObject(reader.ReadToEnd());
-                    List<LibroDTO> librosDTO = new List<LibroDTO>();
-                    int i = 0;
-                    int cantidad = mResponseJSON.docs.Count;
-                    while ((i <= cantidad - 1) && (i < 10))
-                    {
-                        LibroDTO LibroDTO = new LibroDTO();
-                        var bResponseItem = mResponseJSON.docs[i];
-                        LibroDTO.Titulo = HttpUtility.HtmlDecode(bResponseItem.title.ToString());
-                        if (bResponseItem.author_name != null)
-                        {
-                            LibroDTO.Autor = HttpUtility.HtmlDecode(bResponseItem.author_name[0].ToString());
-                        }
-                        else
-                        {
-                            LibroDTO.Autor = "Anonimo";
-                        }
-                        if (bResponseItem.isbn != null)
-                        {
-                            string isbn = HttpUtility.HtmlDecode(bResponseItem.isbn[0].ToString());
-                            string isbnSinX = isbn.Replace("X", "");
-                            LibroDTO.ISBN = Int64.Parse(isbnSinX);
-                        }
-                        else
-                        {
-                            LibroDTO.ISBN = 0;
-                        }
-                        librosDTO.Add(LibroDTO);
-                        i++;
-                    }
-                    return librosDTO;
-                }
+                using Stream responseStream = mResponse.GetResponseStream();
+                StreamReader reader = new StreamReader(responseStream, Encoding.UTF8);
+                dynamic mResponseJSON = JsonConvert.DeserializeObject(reader.ReadToEnd());
+                return (mResponseJSON.docs);
             }
             catch (WebException ex)
             {
@@ -66,13 +41,117 @@ namespace Biblioflash.Manager.API
 
                     System.Console.WriteLine("Error: {0}", mErrorText);
                 }
-                return new List<LibroDTO>();
+                return null;
             }
             catch (Exception ex)
             {
                 System.Console.WriteLine("Error: {0}", ex.Message);
-                return new List<LibroDTO>();
+                return null;
             }
         }
+        public List<LibroDTO> ListarLibrosDTO(dynamic docs)
+        {
+            List<LibroDTO> librosDTO = new List<LibroDTO>();
+            if (docs != null)
+            {                
+                int i = 0;
+                int cantidad = docs.Count;
+                while ((i <= cantidad - 1) && (i < 10))
+                {
+                    LibroDTO LibroDTO = new LibroDTO();
+                    var bResponseItem = docs[i];
+                    LibroDTO.Titulo = HttpUtility.HtmlDecode(bResponseItem.title.ToString());
+                    if (bResponseItem.author_name != null)
+                    {
+                        LibroDTO.Autor = HttpUtility.HtmlDecode(bResponseItem.author_name[0].ToString());
+                    }
+                    else
+                    {
+                        LibroDTO.Autor = "Anonimo";
+                    }
+                    if (bResponseItem.isbn != null)
+                    {
+                        string isbn = HttpUtility.HtmlDecode(bResponseItem.isbn[0].ToString());
+                        string isbnSinX = isbn.Replace("X", "");
+                        LibroDTO.ISBN = Int64.Parse(isbnSinX);
+                    }
+                    else
+                    {
+                        LibroDTO.ISBN = 0;
+                    }
+                    librosDTO.Add(LibroDTO);
+                    i++;
+                }
+            }
+            return librosDTO;
+        }
+
+
+        //public List<LibroDTO> Consulta(string pTituloLibro)
+        //{
+
+        //    System.Net.ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+        //    var url = "http://openlibrary.org/search.json?title=" + pTituloLibro.Replace(" ", "+");
+        //    HttpWebRequest Request = (HttpWebRequest)WebRequest.Create(url);
+        //    try
+        //    {
+        //        WebResponse mResponse = Request.GetResponse();
+        //        using Stream responseStream = mResponse.GetResponseStream();
+        //        StreamReader reader = new StreamReader(responseStream, Encoding.UTF8);
+        //        dynamic mResponseJSON = JsonConvert.DeserializeObject(reader.ReadToEnd());
+        //        return ListarLibrosDTO(mResponseJSON.docs);
+        //    }
+        //    catch (WebException ex)
+        //    {
+        //        WebResponse mErrorResponse = ex.Response;
+        //        using (Stream mResponseStream = mErrorResponse.GetResponseStream())
+        //        {
+        //            StreamReader mReader = new StreamReader(mResponseStream, Encoding.GetEncoding("utf-8"));
+        //            String mErrorText = mReader.ReadToEnd();
+
+        //            System.Console.WriteLine("Error: {0}", mErrorText);
+        //        }
+        //        return new List<LibroDTO>();
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        System.Console.WriteLine("Error: {0}", ex.Message);
+        //        return new List<LibroDTO>();
+        //    }
+        //}
+
+        //public List<LibroDTO> ListarLibrosDTO(dynamic docs)
+        //{
+        //    List<LibroDTO> librosDTO = new List<LibroDTO>();
+        //    int i = 0;
+        //    int cantidad = docs.Count;
+        //    while ((i <= cantidad - 1) && (i < 10))
+        //    {
+        //        LibroDTO LibroDTO = new LibroDTO();
+        //        var bResponseItem = docs[i];
+        //        LibroDTO.Titulo = HttpUtility.HtmlDecode(bResponseItem.title.ToString());
+        //        if (bResponseItem.author_name != null)
+        //        {
+        //            LibroDTO.Autor = HttpUtility.HtmlDecode(bResponseItem.author_name[0].ToString());
+        //        }
+        //        else
+        //        {
+        //            LibroDTO.Autor = "Anonimo";
+        //        }
+        //        if (bResponseItem.isbn != null)
+        //        {
+        //            string isbn = HttpUtility.HtmlDecode(bResponseItem.isbn[0].ToString());
+        //            string isbnSinX = isbn.Replace("X", "");
+        //            LibroDTO.ISBN = Int64.Parse(isbnSinX);
+        //        }
+        //        else
+        //        {
+        //            LibroDTO.ISBN = 0;
+        //        }
+        //        librosDTO.Add(LibroDTO);
+        //        i++;
+        //    }
+        //    return librosDTO;
+        //}
     }
 }
---
+++ TestUnitario/TestsConsultaAPI.cs
@@ -0,0 +1,23 @@
+﻿using Biblioflash.Manager.Domain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace TestUnitario
+{
+    [TestClass]
+    public class TestsConsultaAPI
+    {
+        [TestMethod]
+        public void DiasAtrasados_UnDía_1()
+        {
+            var prestamo = new Prestamo();
+            prestamo.FechaDevolucion = DateTime.Today.AddDays(-1);
+
+            var result = prestamo.diasAtrasados();
+
+            Assert.IsTrue(result == 1);
+        }
+
+    }
+}
